43a09ce3f0cf0bec69d1c3092c09e4f17285912b
HorvathMate04 <culubpenguin@gmail.com>
2024-12-19 00:14:13 +0100

    ...

--- converter.mjs
+++ converter.mjs
@@ -14,7 +14,7 @@ async function getFilePath() {
             resolve(filePath.trim());
         });
     });
-}
+};
 
 async function fetchJSONFile(src) {
     try {
@@ -24,12 +24,66 @@ async function fetchJSONFile(src) {
     } catch (err) {
         console.error("Error reading or parsing the file:", err.message);
         throw err;
-    }
-}
+    },
+};
 
-function geatherDataAboutRecipe(recipe){
-    
-}
+function collectMaterialsForGraph(materials, recipes){
+    let graph = new Set(materials);
+    let elementAdded = true;
+    while(elementAdded && graph.size < 20){
+        Object.keys(recipes).forEach(group => {
+            recipes[group].forEach(recipe => {
+                let mats;
+                if(recipe.shapeless){
+                    mats = recipe.recipe.required;
+                }else{
+                    mats = recipe.recipe;
+                }
+                if(checkForSameMaterial(graph, mats)){
+                    graph = addMaterialsToSet(graph, mats)
+                }
+            });
+        });
+    };
+    return graph.size >= 20;
+};
+
+function checkForSameMaterial(set, mats){
+    mats.forEach(mat => {
+        if(mat != null){
+            if(Array.isArray(mat)){
+                mat.forEach(element => {
+                    if(set.includes(element)){
+                        return true;
+                    };
+                });
+            } else{
+                if(set.includes(mat)){
+                    return true;
+                };
+            };
+        };
+    });
+    return false;
+};
+
+function addMaterialsToSet(set, mats){
+    let result = set;
+    mats.forEach(mat => {
+        if(mat != null){
+            if(Array.isArray(mat)){
+                result.add(mat[Math.floor(Math.random()*mat.length)]);
+            } else{
+                result.add(mat)
+            }
+        };
+    });
+    return result;
+};
+
+function geatherDataAboutRecipe(recipes, recipe){
+    const hasGraphScore20 = collectMaterialsForGraph(recipes);
+};
 
 function analyzeRecipes(recipes){
     Object.keys(recipes).forEach(group => {
@@ -38,18 +92,18 @@ function analyzeRecipes(recipes){
         })
     });
     return true;
-}
+};
 
 async function main() {
     try {
         //const filePath = await getFilePath();
-        const filePath = "./newRecipes.json"
+        const filePath = "./newRecipes.json";
         const recipes = await fetchJSONFile(filePath);
         const analyzedData = analyzeRecipes(recipes.data);
-        console.log(analyzedData)
+        console.log(analyzedData);
     } catch (err) {
         console.error("An error occurred:", err.message);
-    }
-}
+    };
+};
 
 main();
