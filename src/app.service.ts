9731262a3afe7c608e2b1e259d07418c805d353b
FazekasMarton <fazekas.marton16@gmail.com>
2025-02-25 14:46:44 +0100

    game info api added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { CacheService } from './cache/cache.service';
+import * as path from 'path';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private readonly cacheService: CacheService,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getVersion(){
+    const filePath = path.join(__dirname, '../../localData/version.json');
+    return await this.cacheService.loadJsonFile(filePath)
+  }
+
+  async getPatchNotes(){
+    const filePath = path.join(__dirname, '../../localData/patchNotes.json');
+    return await this.cacheService.loadJsonFile(filePath)
+  }
+
+  async getCredits(){
+    const filePath = path.join(__dirname, '../../localData/credits.json');
+    return await this.cacheService.loadJsonFile(filePath)
   }
 }
